aa68d37f79da6cdee1de0388a824c4dbbd7349cb
Dhrubo Hasan Chowdhury <48079397+DHasanC@users.noreply.github.com>
2021-02-23 15:03:07 -0500

    Add files via upload

---
+++ python-notebook_clone-of-python-tutorial-lessons-7-8.py
@@ -0,0 +1,15 @@
+# Python Notebook - Clone of Python Tutorial - Lessons 7 & 8
+
+data = datasets[0]
+
+import pandas as pd
+import numpy as np
+
+data = data.fillna(np.nan)
+
+data.head()
+
+data['delayed'] = data['arr_delay'].apply(lambda x: x > 0) 
+
+
+
